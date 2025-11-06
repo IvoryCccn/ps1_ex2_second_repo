b1cd619e45efdb340f55af74e4fe4263c3ab065d
我是不是有点胖了 <146800928+IvoryCccn@users.noreply.github.com>
2025-11-06 10:53:06 +0000

    Add files via upload

---
+++ _init_.py
@@ -0,0 +1,21 @@
+"""
+GDP Analysis Package
+
+A package for loading, cleaning, and visualizing GDP data for multiple countries.
+"""
+
+from .data import load_gdp_data, clean_gdp_data, get_data_summary
+from .plot_utils import plot_gdp_trends, plot_log_gdp, plot_growth_rates
+
+__version__ = "0.1.0"
+__author__ = "IvoryCccn"
+__email__ = "nc681@cam.ac.uk"
+
+__all__ = [
+    'load_gdp_data',
+    'clean_gdp_data', 
+    'get_data_summary',
+    'plot_gdp_trends',
+    'plot_log_gdp_trends',
+    'plot_growth_rates'
+]
